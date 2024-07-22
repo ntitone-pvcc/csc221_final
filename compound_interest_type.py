6e64a147572d6b22d71b753bd0b7b681263643be
Cory Belew <cab27022@email.vccs.edu>
2024-07-21 21:21:58 -0400

    add documentation

--- compound_interest_type.py
+++ compound_interest_type.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
-
+# A static type that holds the compound rate (times_per_year) and the name of the 
+# interest type. 
 class CompoundInterestType(Enum):
     MONTHLY = ("Monthly", 12)
     QUARTERLY = ("Quarterly", 4)
@@ -15,12 +16,17 @@ class CompoundInterestType(Enum):
     def __str__(self):
         return self._name_
 
+# This type can create a (CompoundInterestType) given a user input of 1, 2, or 3.
+# 1 = MONTHLY
+# 2 = QUARTERLY
+# 3 = YEARLY
 class CompoundInterestTypeWrapper:
     compound_interest_type: CompoundInterestType
 
     def __init__(self, input_string):
         self.compound_interest_type = self._input_value_to_compound_interest_type(input_string)
     
+    # 1, 2, and 3 represent the values shown to the user that correspond with the type
     def _input_value_to_compound_interest_type(self, string):
         if string == '1':
             return CompoundInterestType.MONTHLY
--- investment_length_unit_type.py
+++ investment_length_unit_type.py
@@ -1,27 +1,32 @@
 from enum import Enum
 
+# A static type that holds the compound rate (times_per_year) and the name of the 
+# interest type. 
 class InvestmentLengthUnitType(Enum):
     MONTHS = ("Months", 30)
     QUARTERS = ("Quarters", 90)
     YEARS = ("Years", 365)
 
     days: int
-    years: float
 
     def __init__(self, name, number_of_days):
         self._name_ = name
         self.days = number_of_days
-        self.years = number_of_days/365
 
     def __str__(self):
         return self._name_
-    
+
+# This type can create a (InvestmentLengthUnitType) given a user input of 1, 2, or 3.
+# 1 = MONTHS
+# 2 = QUARTERS
+# 3 = YEARS
 class InvestmentLengthUnitWrapper:
     investment_length_unit_type: InvestmentLengthUnitType
 
     def __init__(self, input_string):
         self.investment_length_unit_type = self._input_value_to_investment_length_unit_type(input_string)
     
+    # 1, 2, and 3 represent the values shown to the user that correspond with the type
     def _input_value_to_investment_length_unit_type(self, string):
         if string == '1':
             return InvestmentLengthUnitType.MONTHS
